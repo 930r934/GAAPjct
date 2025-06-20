c1746667cc318938d05bd9e6ba14c86c0d066bdc
930r934 <930r9399@gmail.com>
2025-06-21 01:54:47 +0530

    added some more logic

--- logExtractScript.js
+++ logExtractScript.js
@@ -1,45 +1,90 @@
 const customLogData = [];
+const customResolutionLogData = [];
+
+//implement type check with square brackets
+const regexNotificationType = /\[(.*?)\]/g;
 
 const regexTitle = /\[FIRING:1\]\s*([\s\S]*?)\s*Firing alerts:/g;
-const regexAlertMessageTime = /(?:AM|PM)\s*([\s\S]*?)\s*\[FIRING:1\]/g;
+const regexResolutionTitle = /\[RESOLVED\]\s*([\s\S]*?)\s*Resolved alerts:/g;
+const regexAlertMessageTime =
+  /(?:AM|PM)\s*([\s\S]*?)\s* (?:\[FIRING:1\]|\[RESOLVED\])/g;
 const regexAlertTime = /at(.{6})/g;
 const regexAlertHostname = "";
 
+let matchNotificationType;
 let matchTitle;
+let matchResolutionTitle;
 let matchAlertMessageTime;
 let matchAlertTime;
 let matchAlertHostname;
 
 let finalData = "";
 
-while ((matchTitle = regexTitle.exec(document.body.innerText)) !== null) {
-  console.log(matchTitle[1]);
+while (
+  (matchNotificationType = regexNotificationType.exec(
+    document.getElementById("logdatadump").innerText
+  )) !== null
+) {
+  console.log(matchNotificationType[1]);
 
-  matchAlertMessageTime = regexAlertMessageTime.exec(document.body.innerText);
-  console.log(matchAlertMessageTime[1]);
+  if (matchNotificationType[1] == "FIRING:1") {
+    matchTitle = regexTitle.exec(
+      document.getElementById("logdatadump").innerText
+    );
+    console.log(matchTitle[1]);
 
-  matchAlertTime = regexAlertTime.exec(document.body.innerText);
-  console.log(matchAlertTime[1]);
+    matchAlertMessageTime = regexAlertMessageTime.exec(
+      document.getElementById("logdatadump").innerText
+    );
+    console.log(matchAlertMessageTime[1]);
 
-  addLog(matchTitle[1], matchAlertMessageTime[1], matchAlertTime[1], "");
-  finalData +=
-    matchTitle[1] +
-    "   " +
-    matchAlertMessageTime[1] +
-    "   " +
-    matchAlertTime[1] +
-    "\n";
+    matchAlertTime = regexAlertTime.exec(
+      document.getElementById("logdatadump").innerText
+    );
+    console.log(matchAlertTime[1]);
+
+    addLog(matchTitle[1], matchAlertMessageTime[1], matchAlertTime[1], "");
+    finalData +=
+      matchTitle[1] +
+      "   " +
+      matchAlertMessageTime[1] +
+      "   " +
+      matchAlertTime[1] +
+      "\n";
+  } else {
+    matchResolutionTitle = regexResolutionTitle.exec(
+      document.getElementById("logdatadump").innerText
+    );
+    console.log(matchResolutionTitle[1]);
+
+    matchAlertMessageTime = regexAlertMessageTime.exec(
+      document.getElementById("logdatadump").innerText
+    );
+    console.log(matchAlertMessageTime[1]);
+
+    matchAlertTime = regexAlertTime.exec(
+      document.getElementById("logdatadump").innerText
+    );
+    console.log(matchAlertTime[1]);
+
+    addResolutionLog(
+      matchResolutionTitle[1],
+      matchAlertMessageTime[1],
+      matchAlertTime[1]
+    );
+  }
 
   // This logs only the content between the two phrases
 }
 
 console.log(finalData);
 renderTableFromCustomLogData();
+renderResolutionTableFromCustomLogData();
 
 // const regex0 = /(?:AM|PM)\s*([\s\S]*?)\s*\[FIRING:1\]/g;
 // let match0;
 // let finalData0 = '';
-// while ((match0 = regex0.exec(document.body.innerText)) !== null) {
+// while ((match0 = regex0.exec(document.getElementById("logdatadump").innerText)) !== null) {
 //     console.log(match0[1]);
 //     finalData0+=match0[1]+'\n';// This logs only the content between the two phrases
 // }
@@ -88,13 +133,64 @@ function addLog(title, alertMessageTime, alertTime, alertHostname) {
   }
 }
 
+function addResolutionLog(title, alertMessageTime, alertTime) {
+  let matchingResolutionTitleRecord = customResolutionLogData.find(
+    (log) => log.title === title
+  );
+
+  if (matchingResolutionTitleRecord) {
+    matchingResolutionTitleRecord.count += 1;
+    matchingResolutionTitleRecord.alertTime += `, ${alertTime}`;
+    matchingResolutionTitleRecord.alertMessageTime += `, ${alertMessageTime}`;
+    console.log(
+      matchingResolutionTitleRecord.minAlertTime,
+      matchingResolutionTitleRecord.maxAlertTime
+    );
+    console.log(alertTime);
+
+    if (
+      toMinutes(matchingResolutionTitleRecord.minAlertTime) >
+      toMinutes(alertTime)
+    ) {
+      matchingResolutionTitleRecord.minAlertTime = alertTime;
+      console.log(
+        "minAlertTime updated to:",
+        matchingResolutionTitleRecord.minAlertTime
+      );
+    }
+    if (
+      toMinutes(matchingResolutionTitleRecord.maxAlertTime) <
+      toMinutes(alertTime)
+    ) {
+      matchingResolutionTitleRecord.maxAlertTime = alertTime;
+      console.log(
+        "maxAlertTime updated to:",
+        matchingResolutionTitleRecord.maxAlertTime
+      );
+    }
+  } else {
+    const logResolutionInput = {
+      title,
+      alertMessageTime,
+      alertTime,
+      count: 1,
+      minAlertTime: alertTime,
+      maxAlertTime: alertTime,
+    };
+
+    console.log(logResolutionInput);
+
+    customResolutionLogData.push(logResolutionInput);
+  }
+}
+
 function renderTableFromCustomLogData() {
   if (customLogData.length === 0) {
     document.body.insertAdjacentHTML("beforeend", "<p>No data to display.</p>");
     return;
   }
 
-  let tableHTML = `<div class="container-md p-4"> <table border="1" cellpadding="5" cellspacing="0" class="table table-bordered border-dark table-striped table-dark table-hover table-hover-color">
+  let tableHTML = `<div class="container-md p-4"> <h2 class="text-light mb-5">Alert's</h2> <table border="1" cellpadding="5" cellspacing="0" class="table table-bordered border-dark table-striped table-dark table-hover table-hover-color">
         <thead>
             <tr>
                 <th>Title</th>
@@ -126,7 +222,45 @@ function renderTableFromCustomLogData() {
   document.getElementById("logtablecontainer").innerHTML = tableHTML;
 }
 
+function renderResolutionTableFromCustomLogData() {
+  let tableHTML = `<div class="container-md p-3"> <h2 class="text-light mb-5">Resolution's</h2>  <table border="1" cellpadding="5" cellspacing="0" class="table table-striped table-bordered border-success table-dark table-hover table-hover-color">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Alert Message Time</th>
+                <th>Alert Times</th>
+                <th>Count</th>
+                <th>Min Alert Time</th>
+                <th>Max Alert Time</th>
+            </tr>
+        </thead>
+        <tbody>`;
+
+  customResolutionLogData.forEach((log) => {
+    tableHTML += `<tr>
+            <td>${log.title}</td>
+            <td>${log.alertMessageTime}</td>
+            <td>${log.alertTime}</td>
+            <td>${log.count}</td>
+            <td>${log.minAlertTime}</td>
+            <td>${log.maxAlertTime}</td>
+        </tr>`;
+  });
+
+  tableHTML += `</tbody></table></div>`;
+
+  document.getElementById("logresolutiontablecontainer").innerHTML = tableHTML;
+}
+
 function toMinutes(timeStr) {
   const [hours, minutes] = timeStr.split(":").map(Number);
   return hours * 60 + minutes;
+}
+
+function produceFinalTable() {
+  customResolutionLogData.forEach((resdata) => {
+    let matchingTitleRecord = customLogData.find(
+      (logdata) => logdata.title === resdata.title
+    );
+  });
 }
